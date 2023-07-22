1b5ec193788d721a2e695ae8a749a7a40fd8f23b
Alex <natorvano@gmail.com>
2023-07-22 11:45:03 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          width={50}
+          height={50}
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
+        <p>Balda will be here</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
